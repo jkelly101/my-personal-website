58c2ae215280951236fb057f1a45729a2af9ee60
Jen <jenLkelly@gmail.com>
2024-02-29 17:39:05 -0500

    updated html content

--- script.js
+++ script.js
@@ -3,3 +3,36 @@
 
 // document.querySelector("body").appendChild(h2);
 
+const list = document.getElementsByClassName("list");
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+list.addEventListener(
+  "mouseenter",
+  (event) => {
+    // highlight the mouseenter target
+    event.target.style.color = "purple";
+
+    // reset the color after a short delay
+    setTimeout(() => {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false,
+);
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+list.addEventListener(
+  "mouseover",
+  (event) => {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(() => {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false,
+);
